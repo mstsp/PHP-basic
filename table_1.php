e133512fa187be6d7039bf1fdb10a9c9f899eb65
Svetlana Tovt <ms.tsp00@gmail.com>
2020-08-31 14:41:04 +0300

    Init commit

---
+++ table_1.php
@@ -0,0 +1,24 @@
+<h1>Table with tag &lt;table&gt;</h1>
+
+<table border="1" cellspacing="0">
+    <tr>
+        <th>Heading 1</th>
+        <th>Heading 2</th>
+        <th>Heading 3</th>
+    </tr>
+    <tr>
+        <td>data 1</td>
+        <td>data 2</td>
+        <td>data 3</td>
+    </tr>
+    <tr>
+        <td>data 1</td>
+        <td>data 2</td>
+        <td>data 3</td>
+    </tr>
+    <tr>
+        <td>data 1</td>
+        <td>data 2</td>
+        <td>data 3</td>
+    </tr>
+</table>
---
+++ table_2.php
@@ -0,0 +1,26 @@
+<link rel='stylesheet' href='./styles/styles.css'>
+
+<h1>Table with tag &lt;div&gt;</h1>
+
+<div class="table">
+    <div class="row heading">
+        <div class="cell">Heading 1</div>
+        <div class="cell">Heading 2</div>
+        <div class="cell">Heading 3</div>
+    </div>
+    <div class="row">
+        <div class="cell">data 1</div>
+        <div class="cell">data 2</div>
+        <div class="cell">data 3</div>
+    </div>
+    <div class="row">
+        <div class="cell">data 1</div>
+        <div class="cell">data 2</div>
+        <div class="cell">data 3</div>
+    </div>
+    <div class="row">
+        <div class="cell">data 1</div>
+        <div class="cell">data 2</div>
+        <div class="cell">data 3</div>
+    </div>
+</div>
