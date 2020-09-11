7837b924414790defa2980d8f2812053beb555f4
Svetlana Tovt <ms.tsp00@gmail.com>
2020-09-11 15:20:37 +0300

    HW_5 finished

--- index.php
+++ index.php
@@ -4,40 +4,41 @@
 $value = 5.555;
 function defineDataType($value = NULL)
 {
-        switch (gettype($value)) {
-            case "integer":
-                echo "Переданно тип данных: integer";
-                break;
-            case "boolean":
-                echo "Переданно тип данных: boolean";
-                break;
-            case "double":
-                echo "Переданно тип данных: float";
-                break;
-            case "string":
-                echo "Переданно тип данных: string";
-                break;
-            case "array":
-                echo "Переданно тип данных: array";
-                break;
-            case "object":
-                echo "Переданно тип данных: object";
-                break;
-            case "resource":
-                echo "Переданно тип данных: resource";
-                break;
-            case "NULL":
-                echo "Ничего не передано";
-                break;
-        }
-
+    switch (gettype($value)) {
+        case "integer":
+            echo "Переданно тип данных: integer";
+            break;
+        case "boolean":
+            echo "Переданно тип данных: boolean";
+            break;
+        case "double":
+            echo "Переданно тип данных: float";
+            break;
+        case "string":
+            echo "Переданно тип данных: string";
+            break;
+        case "array":
+            echo "Переданно тип данных: array";
+            break;
+        case "object":
+            echo "Переданно тип данных: object";
+            break;
+        case "resource":
+            echo "Переданно тип данных: resource";
+            break;
+        case "NULL":
+            echo "Ничего не передано";
+            break;
+    }
 }
+
 defineDataType($value);
 echo "\r\n";
 
 //Создать функцию которая считает все буквы b в переданной строке, в случае если передается не строка функция должна возвращать false
 $string = 'lsdjfdshcslcjasbdbsnbsadab';
-function charCounter($string) {
+function charCounter($string)
+{
     if (gettype($string) === 'string') {
         $counter = substr_count($string, 'b');
         echo "In the string $counter char b" . "\r\n";
@@ -46,8 +47,8 @@ function charCounter($string) {
         echo 'Not string' . "\r\n";
         return false;
     }
-
 }
+
 charCounter($string);
 
 //Создать функцию которая считает сумму значений всех элементов массива произвольной глубины
@@ -75,7 +76,7 @@ $array = [
 function sumArray($array)
 {
     $totalSum = 0;
-    array_walk_recursive( $array, function($value, $key) use (&$totalSum) {
+    array_walk_recursive($array, function ($value, $key) use (&$totalSum) {
         $totalSum += $value;
     }, $totalSum);
     echo "sumArray : $totalSum" . "\r\n";
